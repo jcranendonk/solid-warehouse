f061b5afc0ff47df43bc3e7c10b18e7f268293fe
Jeroen Cranendonk <jeroen.cranendonk@workiva.com>
2015-03-15 20:54:31 -0600

    Added build notifier

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,19 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var notifier = require('node-notifier');
+
+function NotifyPlugin() {}
+NotifyPlugin.prototype.apply = function(compiler) {
+    compiler.plugin('done', function(stats) {
+        var jsonStats = stats.toJson();
+        var errorCount = jsonStats.errors.length;
+
+        notifier.notify({
+            title: 'Sox JS - Watch',
+            message: stats.hasErrors() ? errorCount + ' Build Error(s)' : 'Build Complete',
+            sound: stats.hasErrors() ? 'Funk' : false
+        });
+    });
+};
 
 module.exports = {
     entry: {
@@ -32,6 +47,9 @@ module.exports = {
             test: /\.scss$/,
             loader: ExtractTextPlugin.extract('css!sass')
         }, {
+            test: /\.less$/,
+            loader: ExtractTextPlugin.extract('css!less')
+        }, {
             test: /\/img\//,
             loader: 'file?name=img/[hash].[ext]'
         }, {
@@ -40,6 +58,7 @@ module.exports = {
         }]
     },
     plugins: [
-        new ExtractTextPlugin('css/[name].css')
+        new ExtractTextPlugin('css/[name].css'),
+        new NotifyPlugin()
     ]
 };
